1f196bcfa995565f37a2a74a8e6e1366efc70b96
Andrea Mandini <a.mandini@mauti.it>
2024-12-14 18:43:04 +0100

    chore: add alias for ts and scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component, inject} from '@angular/core';
 import {FontsServiceService} from './shared/fonts-service.service';
+import {TestImportComponent} from '@components/test-import/test-import.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [TestImportComponent],
   standalone: true,
   styleUrl: './app.component.scss'
 })
