faa8074b49eb455e4a42b5e0beb12004c5ab920c
Andrea Mandini <a.mandini@mauti.it>
2024-12-14 18:51:21 +0100

    #1.1 Add aliases (#2)
    * chore: reset.scss
    
    * feat: font import and tests
    
    * feat: app basic styles
    
    * fix (app): missing standalone and remove routing
    
    * chore: install webfontloader library
    
    * feat: fonts-service.service
    
    * chore: test for dynamically font load
    
    * chore: add alias for ts and scss

---
+++ src/app/components/test-import/test-import.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-import',
+  standalone: true,
+  templateUrl: './test-import.component.html',
+  styleUrl: './test-import.component.scss'
+})
+export class TestImportComponent {
+
+}
