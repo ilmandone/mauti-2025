b49084f4a493493f7da30880baf2942b2042c2cd
Andrea Mandini <a.mandini@mauti.it>
2024-12-20 18:00:41 +0100

    feat: history-detail

---
+++ src/app/components/history-detail/history-detail.component.ts
@@ -0,0 +1,26 @@
+import {Component, input} from '@angular/core';
+import {CareerStep} from '../../sections/until-now-section/until-now-section.component';
+
+@Component({
+  selector: 'app-history-detail',
+  standalone: true,
+  template: `
+    <span class="period">{{ data()?.time }}</span>
+    <br>
+    <span class="visually-hidden">Role:</span>
+    <strong>{{ data()?.role }}</strong>
+    <br>
+    <span class="visually-hidden">Company:</span>
+    <span>{{ data()?.company }}</span>
+  `,
+  styles: `
+    strong {
+      font-weight: 700;
+    }
+  `
+})
+export class HistoryDetailComponent {
+
+  data = input<CareerStep>()
+
+}
