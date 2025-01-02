2d6b5cc67245eca06aeca380340d0880e23b6036
Andrea Mandini <a.mandini@mauti.it>
2025-01-02 22:07:16 +0100

    feat: footer with coords and date/time

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {GeoLocationCoords, getGeolocationCoords} from '../../shared/geolocation';
+import {map, Observable, timer} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+
+interface TimeData {
+  day: string
+  month: string
+  year: string
+  hour: string
+  minute: string
+}
 
 @Component({
   selector: 'footer[app-footer]',
@@ -6,6 +17,38 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  private _destroyRef = inject(DestroyRef)
+
+  geoLocationCoords!: GeoLocationCoords
+  timeData!: TimeData
+
+  /**
+   * Return and observable that each minute give the current day and time
+   * @private
+   */
+  private getCurrentTimeObservable(): Observable<TimeData> {
+    return timer(0, 60000).pipe(
+      map(() => {
+        const now = new Date()
+        return {
+          day: now.getDate().toString().padStart(2, '0'),
+          month: (now.getUTCMonth() + 1).toString().padStart(2, '0'),
+          year: now.getFullYear().toString(),
+          hour: now.getHours().toString().padStart(2,'0'),
+          minute: now.getMinutes().toString().padStart(2,'0')
+        }
+      })
+    )
+  }
+
+  ngOnInit() {
+    this.geoLocationCoords = getGeolocationCoords()
+    this.getCurrentTimeObservable().pipe(
+      takeUntilDestroyed(this._destroyRef)
+    ).subscribe(r => {
+      this.timeData = r
+    })
+  }
 }
