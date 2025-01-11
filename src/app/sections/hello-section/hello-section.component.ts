1fd70b4f4da088b2cb8106edc8473dba4b5b665c
Andrea Mandini <a.mandini@mauti.it>
2025-01-11 17:07:58 +0100

    chore: revert block-to-text.directive.ts

--- src/app/sections/hello-section/hello-section.component.ts
+++ src/app/sections/hello-section/hello-section.component.ts
@@ -1,13 +1,11 @@
-import {Component, computed, inject} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {InViewportDirective} from '../../shared/directives/in-viewport.directive';
 import {StateService} from '../../shared/services/state.service';
-import {BlockToTextDirective} from '../../shared/directives/block-to-text.directive';
 
 @Component({
   selector: 'app-hello-section',
   imports: [
     InViewportDirective,
-    BlockToTextDirective
   ],
   standalone: true,
   templateUrl: './hello-section.component.html',
@@ -16,10 +14,6 @@ import {BlockToTextDirective} from '../../shared/directives/block-to-text.direct
 export class HelloSectionComponent {
 
   private _stateSrv = inject(StateService)
-
-  visible = computed(() => {
-    return this._stateSrv.section() === 'hello'
-  })
 
   visibilityChange($event: boolean) {
     if ($event)
