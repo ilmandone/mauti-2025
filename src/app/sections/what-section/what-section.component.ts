c4039111a3e0f156381db2879a3997411d29f68e
Andrea Mandini <a.mandini@mauti.it>
2025-01-21 13:45:02 +0100

    feat: start animation for what section

--- src/app/sections/what-section/what-section.component.ts
+++ src/app/sections/what-section/what-section.component.ts
@@ -1,22 +1,36 @@
 import {Component, inject} from '@angular/core';
 import {InViewportDirective} from '../../shared/directives/in-viewport.directive';
 import {StateService} from '../../shared/services/state.service';
+import {HiddenTextDirective} from '../../shared/directives/hidden-text.directive';
 
 @Component({
   selector: 'app-what-section',
   imports: [
-    InViewportDirective
+    InViewportDirective,
+    HiddenTextDirective
   ],
   standalone: true,
   templateUrl: './what-section.component.html',
   styleUrl: './what-section.component.scss'
 })
 export class WhatSectionComponent {
-
   private _stateSrv = inject(StateService)
+  private _visible = false
+
+  sectionVisible = false
+
+  private _showSequence() {
+    this._visible = true
+
+    window.setTimeout(() => {
+      this.sectionVisible = true
+    },200)
+  }
 
   visibilityChange($event: boolean) {
-    if ($event)
+    if ($event) {
       this._stateSrv.setSection('what')
+      if (!this._visible) this._showSequence()
+    }
   }
 }
