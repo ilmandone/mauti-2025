ab3391e50dfa8ef43bbca07a9b13a7d91daf8ff8
Andrea Mandini <a.mandini@mauti.it>
2025-01-21 22:00:59 +0100

    feat (what): complete show animation

--- src/app/sections/what-section/what-section.component.ts
+++ src/app/sections/what-section/what-section.component.ts
@@ -1,11 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {InViewportDirective} from '../../shared/directives/in-viewport.directive';
 import {StateService} from '../../shared/services/state.service';
+import {TextScrambleLeftRightDirective} from '../../shared/directives/text-scramble-left-right.directive';
+import {checkMobile} from '../../shared/detect.mobile';
 
 @Component({
   selector: 'app-what-section',
   imports: [
-    InViewportDirective
+    InViewportDirective,
+    TextScrambleLeftRightDirective
   ],
   standalone: true,
   templateUrl: './what-section.component.html',
@@ -13,22 +16,22 @@ import {StateService} from '../../shared/services/state.service';
 })
 export class WhatSectionComponent {
   private _stateSrv = inject(StateService)
-  private _visible = false
+  private _isMobile = checkMobile()
 
-  sectionVisible = false
-
-  private _showSequence() {
-    this._visible = true
-
-    window.setTimeout(() => {
-      this.sectionVisible = true
-    },200)
-  }
+  feVisible = false
+  uxVisible = false
+  cgVisible = false
 
   visibilityChange($event: boolean) {
     if ($event) {
       this._stateSrv.setSection('what')
-      if (!this._visible) this._showSequence()
     }
+  }
+
+  subVisibilityChange($event: number, index: string, cv: boolean) {
+    const t = this._isMobile ? 0.25 : 0.6
+    if ($event > t && !cv) {
+       Object.assign(this, {[`${index}Visible`]: true})
+     }
   }
 }
