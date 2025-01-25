9ea121d8c2367d3e5320f38ccb6b7e9ec9921949
Andrea Mandini <a.mandini@mauti.it>
2025-01-25 14:58:00 +0100

    feat (infinite-scroll): add touch detection

--- src/app/shared/directives/infinite-scroll.directive.ts
+++ src/app/shared/directives/infinite-scroll.directive.ts
@@ -7,13 +7,41 @@ export class InfiniteScrollDirective {
 
   private _elementRef = inject(ElementRef)
   private _scrollValue = 0
+  private _scrollValueNext = 0
+  private _startY = 0
+
+  private _updateElTranslate(v: number) {
+    const nEl = this._elementRef.nativeElement as HTMLElement
+    nEl.style.transform = `translateY(${v}px)`
+  }
 
   @HostListener('window:wheel', ['$event'])
   onWindowScroll(event: WheelEvent): void {
     // Your logic here
-    const nEl = this._elementRef.nativeElement as HTMLElement
     this._scrollValue -= event.deltaY
-    nEl.style.transform = `translateY(${this._scrollValue}px)`
+    this._updateElTranslate(this._scrollValue)
   }
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event$: TouchEvent) {
+    this._startY = event$.targetTouches[0].clientY
+
+  }
+
+  @HostListener('touchmove', ['$event'])
+  onTouchMove(event$: TouchEvent) {
+    const delta = this._startY - event$.targetTouches[0].clientY
+    this._scrollValueNext = this._scrollValue - delta
+
+    this._updateElTranslate(this._scrollValueNext)
+  }
+
+  @HostListener('touchend')
+  onTouchEnd() {
+    this._scrollValue = this._scrollValueNext
+    this._scrollValueNext = 0
+  }
+
+
 
 }
