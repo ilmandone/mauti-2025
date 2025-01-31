bcc6718d37c0000cbb7215a4d334046213fc3ac6
Andrea Mandini <a.mandini@mauti.it>
2025-01-31 21:59:27 +0100

    feat: svg noise filter component

---
+++ src/app/components/noise-svg/noise-svg.component.ts
@@ -0,0 +1,43 @@
+import { Component, computed, input } from '@angular/core';
+
+@Component({
+  selector: 'noise-svg-filter',
+  imports: [],
+  template: `
+    <svg xmlns="http://www.w3.org/2000/svg" class="svg-filter">
+      <defs>
+        <filter id="noise-footer" x="0" y="0" height="100%">
+          <feOffset in="SourceGraphic" dx="-8" dy="-8" result="offset" />
+          <feGaussianBlur in="offset" stdDeviation="32" result="blur" />
+          <feTurbulence result="waves" type="turbulence" baseFrequency="0.735 0.771" numOctaves="1"
+                        seed="256"></feTurbulence>
+          <feDisplacementMap in="blur" in2="waves" scale="120" xChannelSelector="R" yChannelSelector="B"
+                             result="ripples"></feDisplacementMap>
+          <feComposite in="waves" in2="ripples" operator="arithmetic" k1="1" k2="0" k3="1" k4="0"></feComposite>
+        </filter>
+        <filter id="noise-header" x="0" y="0" height="60%">
+          <feOffset in="SourceGraphic" dx="-8" dy="-8" result="offset" />
+          <feGaussianBlur in="offset" stdDeviation="48" result="blur" />
+          <feTurbulence result="waves" type="turbulence" baseFrequency="0.435 0.771" numOctaves="2"
+                        seed="256"></feTurbulence>
+          <feDisplacementMap in="blur" in2="waves" scale="-120" xChannelSelector="R" yChannelSelector="B"
+                             result="ripples"></feDisplacementMap>
+          <feComposite in="waves" in2="ripples" operator="arithmetic" k1="1" k2="0" k3="1" k4="0"></feComposite>
+        </filter>
+      </defs>
+    </svg>
+  `,
+  styles:`
+    :host {
+      position: absolute;
+      display: block;
+
+      .svg-filter {
+        position: absolute;
+        width: 1px;
+        height: 1px;
+      }
+    }
+  `
+})
+export class NoiseSvgComponent {}
