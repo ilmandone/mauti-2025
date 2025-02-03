a24beb5d5aa9d96bc131480c994c751e2a400ce8
Andrea Mandini <a.mandini@mauti.it>
2025-02-03 21:57:05 +0100

    feat (header): add navigation to and from about

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,25 @@
 import { Component, computed, effect, inject } from '@angular/core';
 import { ButtonComponent } from '@components/button/button.component';
 import { checkMobile } from '../../shared/detect.mobile';
-import { FontsService } from '../../shared/services/fonts.service';
 import { StateService } from '../../shared/services/state.service';
 import { NoiseSvgComponent } from '@components/noise-svg/noise-svg.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'header[app-header]',
-  imports: [ButtonComponent, NoiseSvgComponent],
+  imports: [ButtonComponent, NoiseSvgComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  private _fontSrv = inject(FontsService);
+  // private _fontSrv = inject(FontsService);
   private _stateSrv = inject(StateService);
 
-  fontFamily = computed(() => {
+  currentSection = computed(() => this._stateSrv.section());
+
+  /*fontFamily = computed(() => {
     return this._fontSrv.fontData().fontFamilyName.toUpperCase();
-  });
+  });*/
 
   isMobile = checkMobile();
   show = false;
