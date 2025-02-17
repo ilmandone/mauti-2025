0f9d144e920ed1b7fab461527fc9a6d9ce258ea4
Andrea Mandini <a.mandini@mauti.it>
2025-02-17 20:40:23 +0100

    chore: create ack component

---
+++ src/app/pages/ack/ack.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ack',
+  imports: [],
+  templateUrl: './ack.component.html',
+  styleUrl: './ack.component.scss'
+})
+class AckComponent {
+
+}
+
+export default AckComponent
