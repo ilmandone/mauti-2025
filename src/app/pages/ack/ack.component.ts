063561566efde94219fadd2805af9cc2aa222d8f
Andrea Mandini <a.mandini@mauti.it>
2025-02-17 22:24:10 +0100

    feat (ack): text splits and animation

--- src/app/pages/ack/ack.component.ts
+++ src/app/pages/ack/ack.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonComponent } from '@components/button/button.component';
+import { TextScrambleLeftRightDirective } from '../../shared/directives/text-scramble-left-right.directive';
 
 @Component({
   selector: 'app-ack',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, TextScrambleLeftRightDirective],
   templateUrl: './ack.component.html',
   styleUrl: './ack.component.scss',
 })
-class AckComponent {}
+class AckComponent implements OnInit {
+  visible = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.visible = true;
+    }, 400);
+  }
+}
 
 export default AckComponent
