7d9802faee98c8570b897caa98044b50c7ee9fe8
Andrea Mandini <a.mandini@mauti.it>
2025-02-20 20:27:57 +0100

    feat: add chars to char spinner

--- src/app/components/char-spinner/char-spinner.component.ts
+++ src/app/components/char-spinner/char-spinner.component.ts
@@ -1,74 +1,87 @@
-import {
-  AfterViewInit,
-  Component,
-  DestroyRef,
-  ElementRef,
-  HostBinding,
-  inject,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
-import { debounceTime, fromEvent, Subscription, takeUntil } from 'rxjs';
+import { Component, DestroyRef, ElementRef, inject, OnInit } from '@angular/core';
+import { debounceTime, fromEvent } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 interface PosCoord {
-  x: number
-  y: number
+  x: number;
+  y: number;
 }
 
 @Component({
   selector: 'char-spinner',
   imports: [],
-  template: ` <div>{{ currentAngle }}</div> `,
+  template: ` <div>{{ CHAR_MAP[currentAngle] }}</div> `,
   styleUrl: './char-spinner.component.scss',
 })
 export class CharSpinnerComponent implements OnInit {
-  private _dRef = inject(DestroyRef)
+  readonly CHAR_MAP: Record<number, string> = {
+    0: '|',
+    45: '/',
+    90: '-',
+    135: '\\',
+    180: '|',
+    225: '/',
+    270: '-',
+    315: '\\',
+  };
+
+  private _dRef = inject(DestroyRef);
   private _nEl = inject(ElementRef).nativeElement as HTMLElement;
 
-  private _elPos!: PosCoord
-  private _mouseEvt$ = fromEvent<MouseEvent>(document, 'mousemove')
-  private _resizeEvt$ = fromEvent(window, 'resize').pipe(debounceTime(150))
+  private _elPos!: PosCoord;
+  private _mouseEvt$ = fromEvent<MouseEvent>(document, 'mousemove');
+  private _resizeEvt$ = fromEvent(window, 'resize').pipe(debounceTime(150));
 
   currentAngle = 0;
 
-  private _getElementPosition():PosCoord {
-    const bb = this._nEl.getBoundingClientRect()
-    console.log(bb);
-    console.log(this._nEl);
-    return {
-      x: bb.x + bb.width / 2,
-      y: bb.y + bb.height / 2,
-    }
-  }
-
-  private _getAngle(event:MouseEvent) {
+  /**
+   * Get angle related to component position
+   * @description The value is a multiple of 45 degrees
+   * @param {MouseEvent} event
+   * @private
+   */
+  private _getSnapAngle(event: MouseEvent) {
     const deltaX = event.clientX - this._elPos.x;
     const deltaY = event.clientY - this._elPos.y;
 
     const angle = Math.atan2(deltaY, deltaX) + Math.PI / 2;
-    this.currentAngle = this._snapTo45Degrees(Math.round(((angle * 180 / Math.PI) + 360) % 360))
+    this.currentAngle = this._snapTo45Degrees(Math.round(((angle * 180) / Math.PI + 360) % 360));
   }
 
+  /**
+   * Get element center position
+   * @private
+   */
+  private _getElCenterPosition(): PosCoord {
+    const bb = this._nEl.getBoundingClientRect();
+    return {
+      x: bb.x + bb.width / 2,
+      y: bb.y + bb.height / 2,
+    };
+  }
+
+  /**
+   * Snap angle value to 45 degrees multiple
+   * @param angle
+   * @private
+   */
   private _snapTo45Degrees(angle: number): number {
     const snapped = Math.round(angle / 45) * 45;
     return snapped === 360 ? 0 : snapped;
   }
 
   ngOnInit() {
-
     // Initialize the component after 100 ms to get correct size and position
     // Note: an external interceptor observer could be used to initialize the component
 
     setTimeout(() => {
-      this._elPos = this._getElementPosition()
-      this._mouseEvt$.pipe(takeUntilDestroyed(this._dRef)).subscribe(r => {
-        this._getAngle(r)
-      })
-      this._resizeEvt$.pipe(takeUntilDestroyed(this._dRef)).subscribe(r => {
-        this._elPos = this._getElementPosition()
-      })
-    },100)
-
+      this._elPos = this._getElCenterPosition();
+      this._mouseEvt$.pipe(takeUntilDestroyed(this._dRef)).subscribe((r) => {
+        this._getSnapAngle(r);
+      });
+      this._resizeEvt$.pipe(takeUntilDestroyed(this._dRef)).subscribe((r) => {
+        this._elPos = this._getElCenterPosition();
+      });
+    }, 100);
   }
 }
