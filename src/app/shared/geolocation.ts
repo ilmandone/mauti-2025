de4ca1d2c05429415133bba74cfca68e286b0454
Andrea Mandini <a.mandini@mauti.it>
2025-02-21 23:00:14 +0100

    fix (geolocation): getGeolocationCoords return observable

--- src/app/shared/geolocation.ts
+++ src/app/shared/geolocation.ts
@@ -1,30 +1,38 @@
+import { Observable, of } from 'rxjs';
+
 export interface GeoLocationCoords {
-  lat: number,
-  lon: number
+  lat: number;
+  lon: number;
 }
 
-export const getGeolocationCoords = (): GeoLocationCoords => {
-  const geolocation = navigator.geolocation
-  const defaultPosition = {lat: 44.4598629, lon: 11.1955072}
-  const noPosMsg = 'Unable to get your position :('
+/**
+ * Return a cold observable that return user or default position
+ * @param enabled
+ */
+export const getGeolocationCoords = (enabled: boolean): Observable<GeoLocationCoords> => {
+  const geolocation = navigator.geolocation;
+  const defaultPosition = { lat: 44.4598629, lon: 11.1955072 };
+  const noPosMsg = 'Use default position';
 
-  if (geolocation) {
-    let p = defaultPosition
+  if (!navigator.geolocation || !enabled) {
+    return of(defaultPosition);
+  }
+
+  return new Observable<GeoLocationCoords>((subscriber) => {
     geolocation.getCurrentPosition(
       (position) => {
-        p = {
+        subscriber.next({
           lat: position.coords.latitude,
-          lon: position.coords.longitude
-        }
+          lon: position.coords.longitude,
+        });
+        subscriber.complete();
       },
       (error) => {
-        console.error(error.message)
-        console.warn(noPosMsg)
+        console.warn(noPosMsg);
+        console.error(error);
+        subscriber.next(defaultPosition);
+        subscriber.complete();
       }
-    )
-    return p
-  } else {
-    console.warn(noPosMsg)
-    return defaultPosition
-  }
-}
+    );
+  });
+};
