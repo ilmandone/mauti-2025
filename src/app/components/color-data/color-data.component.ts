744e3563380338df632c83f0514a6fe5282c396c
Andrea Mandini <a.mandini@mauti.it>
2025-02-26 22:33:01 +0100

    feat (about): color in vertical mode

--- src/app/components/color-data/color-data.component.ts
+++ src/app/components/color-data/color-data.component.ts
@@ -4,18 +4,15 @@ import { Component, input } from '@angular/core';
   selector: 'app-color-data',
   imports: [],
   template: `
-    <div class="main" [class]="color()">
-      <ng-content/>
+    <div class="main" [class]="color()" [class.vertical]="orientation() === 'vertical'">
+      <ng-content />
     </div>
-    <div class="def" [class]="color()">
-      //////// {{def()}}
-    </div>
-
+    <div class="def" [class]="color()">//////// {{ def() }}</div>
   `,
   styleUrl: './color-data.component.scss',
 })
 export class ColorDataComponent {
-  def = input<string>('')
-  color = input<'primary' | 'secondary'>('primary')
-  orientation = input<'hor' | 'ver'>('hor')
+  def = input<string>('');
+  color = input<'primary' | 'secondary'>('primary');
+  orientation = input<'horizontal' | 'vertical'>('horizontal');
 }
