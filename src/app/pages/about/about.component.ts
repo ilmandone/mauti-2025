cdc59e6f22ea62634a27e7453189032c30643cd1
Andrea Mandini <a.mandini@mauti.it>
2025-02-26 19:13:03 +0100

    feat (about): history section

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
+import { CareerStep } from '../../sections/until-now-section/until-now-section.component';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 class AboutComponent {
-
+  history: CareerStep[] = [
+    {
+      time: 'Today',
+      company: 'XTel',
+      role: 'Senior front-end developer',
+    },
+    {
+      time: '2021 - 2024',
+      company: 'Var Group',
+      role: 'Front-end architect / senior developer',
+    },
+    {
+      time: '2018 - 2021',
+      company: 'YNAP / Yoox',
+      role: 'Senior front-end developer',
+    },
+    {
+      time: '2015 - 2018',
+      company: 'Life Longari Loman',
+      role: 'Digital Art Director - UX / UI designer',
+    },
+    {
+      time: '2013 - 2015',
+      company: 'Neri Wolff - Quadrante',
+      role: 'UX / UI designer',
+    },
+    {
+      time: '2011 - 2013',
+      company: 'Net Sinergy',
+      role: 'UX / UI designer - Front-end developer',
+    },
+  ];
 }
 
-export default AboutComponent
+export default AboutComponent;
