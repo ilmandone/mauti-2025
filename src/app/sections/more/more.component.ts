87252bddf0aea847074b95dc60e860e7d998c0f8
Andrea Mandini <a.mandini@mauti.it>
2025-03-02 21:27:26 +0100

    feat (more): show animation

--- src/app/sections/more/more.component.ts
+++ src/app/sections/more/more.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, effect, HostBinding, input } from '@angular/core';
+import { TextScrambleLeftRightDirective } from '../../shared/directives/text-scramble-left-right.directive';
 
 @Component({
   selector: 'section[more]',
-  imports: [],
+  imports: [TextScrambleLeftRightDirective],
   templateUrl: './more.component.html',
   styleUrl: './more.component.scss',
 })
-export class MoreComponent {}
+export class MoreComponent {
+  visible = input<boolean>(false);
+
+  @HostBinding('class.visible') isVisible!: boolean;
+
+  constructor() {
+    effect(() => {
+      this.isVisible = this.visible();
+    });
+  }
+}
