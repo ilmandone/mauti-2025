97c23d7b91f75a0ee3cb498617ab45cdbece23dc
Andrea Mandini <a.mandini@mauti.it>
2025-03-05 22:34:55 +0100

    feat: hud glass color and alpha input

--- src/app/shared/directives/hud-glass-shadow.directive.ts
+++ src/app/shared/directives/hud-glass-shadow.directive.ts
@@ -12,31 +12,33 @@ export class HudGlassShadowDirective implements AfterViewInit {
   private _resizeEvt$ = fromEvent(window, 'resize').pipe(debounceTime(50));
 
   update = input<unknown>();
+  hudColor = input<string>('246, 21, 57');
+  hudAlpha = input<number>(0.4);
 
   private _update() {
     const iw = window.innerWidth;
     const ih = window.innerHeight;
-    const vh = Math.round(ih / 200);
+    const vh = Math.round((iw < ih ? iw : ih) / 200);
     const bb = this._el.getBoundingClientRect();
     const c = {
-      x: -Math.round((bb.left + bb.width / 2 - iw / 2) / 60),
-      y: Math.round((bb.top + bb.height / 2 - ih / 2) / 60),
+      x: -Math.round((bb.left + bb.width / 2 - iw / 2) / (iw / 80)),
+      y: Math.round((bb.top + bb.height / 2 - ih / 2) / (ih / 60)),
     };
 
     this._el.style.filter = `
-      drop-shadow(${c.x / 2}px ${c.y / 2 + vh}px 0 rgba(255,0,0,0.4))
-      drop-shadow(${c.x}px ${c.y + vh}px 2px rgba(255,0,0,0.3))
+      drop-shadow(${c.x / 2}px ${c.y / 2 + vh}px 1px rgba(${this.hudColor()}, ${this.hudAlpha()}))
+      drop-shadow(${c.x}px ${c.y + vh}px 3px rgba(${this.hudColor()}, ${this.hudAlpha() / 2}))
       `;
   }
 
   constructor() {
     effect(() => {
-      console.log(this.update());
       if (this.update()) this._update();
     });
   }
 
   ngAfterViewInit() {
+    this._el.classList.add('hud-glass-transition');
     this._resizeEvt$.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(() => {
       this._update();
     });
