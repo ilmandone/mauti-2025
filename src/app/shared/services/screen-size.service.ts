9d0a6fca6b62e0ed9bdffd4cffda927eab706a18
Andrea Mandini <a.mandini@mauti.it>
2025-03-09 18:13:23 +0100

    feat (screen-size.service): screen orientation

--- src/app/shared/services/screen-size.service.ts
+++ src/app/shared/services/screen-size.service.ts
@@ -1,13 +1,14 @@
 import { DestroyRef, inject, Injectable, signal } from '@angular/core';
 import { debounceTime, fromEvent } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ScreenOrientation } from '../commons';
 
 /**
  * Export screen sizes type and values
  */
 const _screenSizes = ['t', 'tl', 'd', 'dm', 'dl', 'dxl'] as const;
 export type ScreenSize = (typeof _screenSizes)[number];
-export const SCREEN_SIZE: ScreenSize[] = [..._screenSizes];
+export const SCREEN_SIZES: ScreenSize[] = [..._screenSizes];
 
 /**
  * Service that return the actual screen size from the css var --screen-size
@@ -18,16 +19,25 @@ export const SCREEN_SIZE: ScreenSize[] = [..._screenSizes];
 })
 export class ScreenSizeService {
   private _destroyRef = inject(DestroyRef);
-  private _screenSizes = SCREEN_SIZE;
+  private _screenSizes = SCREEN_SIZES;
+
+  private _screenSize = signal<ScreenSize | null>(null);
+  private _screenOrientation = signal<ScreenOrientation | null>(null);
 
   private _getScreenSize = () => {
     return getComputedStyle(document.documentElement).getPropertyValue('--screen-size') as ScreenSize;
   };
 
-  private _screenSize = signal<ScreenSize | null>(null);
+  private _getScreenOrientation(): ScreenOrientation {
+    return window.innerWidth / window.innerHeight > 1 ? 'horizontal' : 'vertical';
+  }
 
   get screenSize() {
     return this._screenSize.asReadonly();
+  }
+
+  get screenOrientation() {
+    return this._screenOrientation.asReadonly();
   }
 
   relatedTo(s: ScreenSize): 'equal' | 'after' | 'before' {
@@ -41,13 +51,14 @@ export class ScreenSizeService {
 
   init() {
     this._screenSize.set(this._getScreenSize());
+    this._screenOrientation.set(this._getScreenOrientation());
 
     fromEvent(window, 'resize')
       .pipe(takeUntilDestroyed(this._destroyRef), debounceTime(100))
       .subscribe(() => {
         const sz = this._getScreenSize() || 'm';
-
         if (sz !== this._screenSize()) this._screenSize.set(sz);
+        this._screenOrientation.set(this._getScreenOrientation());
       });
   }
 }
