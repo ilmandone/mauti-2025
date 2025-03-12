3430f3107eb5643ef1e0eaffe8716f24186577da
Andrea Mandini <a.mandini@mauti.it>
2025-03-12 21:08:26 +0100

    feat (hud-glass): add color shift and displacement

--- src/app/shared/directives/hud-glass-shadow.directive.ts
+++ src/app/shared/directives/hud-glass-shadow.directive.ts
@@ -15,20 +15,29 @@ export class HudGlassShadowDirective implements AfterViewInit {
   hudColor = input<string>('246, 21, 57');
   hudAlpha = input<number>(0.4);
 
+  private _getBlurFactor(x: number): number {
+    const xAbs = Math.pow(Math.abs(x * 0.05), 1.5);
+    return Math.round(xAbs);
+  }
+
   private _update() {
     const iw = window.innerWidth;
     const ih = window.innerHeight;
-    const vh = Math.round((iw < ih ? iw : ih) / 200);
+    const vh = Math.round((iw < ih ? iw : ih) / 400);
     const bb = this._el.getBoundingClientRect();
     const c = {
       x: -Math.round((bb.left + bb.width / 2 - iw / 2) / (iw / 80)),
       y: Math.round((bb.top + bb.height / 2 - ih / 2) / (ih / 60)),
     };
 
+    const halfCX = c.x * 0.5;
+    const bf = this._getBlurFactor(c.x);
+
     this._el.style.filter = `
-      drop-shadow(${c.x / 2}px ${c.y / 2 + vh}px 1px rgba(${this.hudColor()}, ${this.hudAlpha()}))
-      drop-shadow(${c.x}px ${c.y + vh}px 3px rgba(${this.hudColor()}, ${this.hudAlpha() / 2}))
-      `;
+      blur(${bf}px)
+      drop-shadow(${halfCX}px ${c.y / 2 - vh}px 1px rgba(${this.hudColor()}, ${this.hudAlpha()}))
+      drop-shadow(${-halfCX}px ${c.y + vh}px 2px var(--hud-color))`;
+    this._el.style.transform = `translateZ(${Math.abs(c.x) * (iw * 0.0025)}px) rotateY(${halfCX}deg)`;
   }
 
   constructor() {
@@ -42,6 +51,12 @@ export class HudGlassShadowDirective implements AfterViewInit {
     this._resizeEvt$.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(() => {
       this._update();
     });
+
+    const pn = this._el.parentNode as HTMLElement;
+    if (pn) {
+      pn.style.perspective = `50vw`;
+      pn.style.transformStyle = 'preserve-3D';
+    }
 
     setTimeout(() => {
       this._update();
