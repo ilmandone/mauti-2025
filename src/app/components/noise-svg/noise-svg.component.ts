d2fa58b98251edd455cf3fc09fe725a9a1709804
Andrea Mandini <a.mandini@mauti.it>
2025-03-12 22:40:40 +0100

    feat (footer | header): fix header and footer backdrop filters

--- src/app/components/noise-svg/noise-svg.component.ts
+++ src/app/components/noise-svg/noise-svg.component.ts
@@ -7,27 +7,21 @@ import { Component } from '@angular/core';
     <svg xmlns="http://www.w3.org/2000/svg" class="svg-filter">
       <defs>
         <filter id="noise-footer" x="0" y="0" height="100%">
-          <feOffset in="SourceGraphic" dx="-8" dy="-8" result="offset" />
-          <feGaussianBlur in="offset" stdDeviation="32" result="blur" />
-          <feTurbulence result="waves" type="turbulence" baseFrequency="0.735 0.771" numOctaves="1"
-                        seed="256"></feTurbulence>
-          <feDisplacementMap in="blur" in2="waves" scale="120" xChannelSelector="R" yChannelSelector="B"
-                             result="ripples"></feDisplacementMap>
-          <feComposite in="waves" in2="ripples" operator="arithmetic" k1="1" k2="0" k3="1" k4="0"></feComposite>
-        </filter>
-        <filter id="noise-header" x="0" y="0" height="60%">
-          <feOffset in="SourceGraphic" dx="-8" dy="-8" result="offset" />
-          <feGaussianBlur in="offset" stdDeviation="72" result="blur" />
-          <feTurbulence result="waves" type="turbulence" baseFrequency="0.735 0.771" numOctaves="2"
-                        seed="256"></feTurbulence>
-          <feDisplacementMap in="blur" in2="waves" scale="-120" xChannelSelector="R" yChannelSelector="B"
-                             result="ripples"></feDisplacementMap>
-          <feComposite in="waves" in2="ripples" operator="arithmetic" k1="1" k2="0" k3="1" k4="0"></feComposite>
+          <feOffset in="SourceGraphic" dx="8" dy="-8" result="offset" />
+          <feGaussianBlur in="offset" stdDeviation="8" result="blur" />
+          <feDisplacementMap
+            in="SourceGraphic"
+            in2="waves"
+            scale="60"
+            xChannelSelector="G"
+            yChannelSelector="B"
+            result="ripples"
+          ></feDisplacementMap>
         </filter>
       </defs>
     </svg>
   `,
-  styles:`
+  styles: `
     :host {
       position: absolute;
       display: block;
@@ -38,6 +32,6 @@ import { Component } from '@angular/core';
         height: 1px;
       }
     }
-  `
+  `,
 })
 export class NoiseSvgComponent {}
