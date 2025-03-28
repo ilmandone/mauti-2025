b426693985e8cbf47b3a05a5a6f7751537ee3589
Andrea Mandini <a.mandini@mauti.it>
2025-03-28 21:53:45 +0100

    feat: home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HelloComponent } from '../../sections/hello/hello.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HelloComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export default class HomeComponent {
-
-}
+export default class HomeComponent {}
