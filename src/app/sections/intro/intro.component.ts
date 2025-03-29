7cbada2a2db2fbe81a8680c61fa32383460df482
Andrea Mandini <a.mandini@mauti.it>
2025-03-29 21:42:34 +0100

    feat: add about text and logo block to intro component

--- src/app/sections/intro/intro.component.ts
+++ src/app/sections/intro/intro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ColorCardComponent } from '@components/color-card/color-card.component';
+import { LogoBlockComponent } from '@components/logo-block/logo-block.component';
 
 @Component({
   selector: 'app-intro',
-  imports: [ColorCardComponent],
+  imports: [ColorCardComponent, LogoBlockComponent],
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
 })
