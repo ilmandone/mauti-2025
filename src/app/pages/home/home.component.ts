466d75bc5f7f3123e1fffa223095f7705f0c7726
Andrea Mandini <a.mandini@mauti.it>
2025-03-29 18:19:14 +0100

    feat: intro component main text

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HelloComponent } from '../../sections/hello/hello.component';
+import { IntroComponent } from '../../sections/intro/intro.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HelloComponent],
+  imports: [HelloComponent, IntroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
