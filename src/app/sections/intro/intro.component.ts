270075f3dd60e6b69a859075c9101dc5a7886cb7
Andrea Mandini <a.mandini@mauti.it>
2025-03-29 19:11:19 +0100

    feat: add color card to intro

--- src/app/sections/intro/intro.component.ts
+++ src/app/sections/intro/intro.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ColorCardComponent } from '@components/color-card/color-card.component';
 
 @Component({
   selector: 'app-intro',
-  imports: [],
+  imports: [ColorCardComponent],
   templateUrl: './intro.component.html',
-  styleUrl: './intro.component.scss'
+  styleUrl: './intro.component.scss',
 })
-export class IntroComponent {
-
-}
+export class IntroComponent {}
