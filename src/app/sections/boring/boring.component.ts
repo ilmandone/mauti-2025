db3e0ae84874d8cd47171d6c33a832f6b51fb089
Andrea Mandini <a.mandini@mauti.it>
2025-04-02 22:18:39 +0200

    feat: boring section with career steps

--- src/app/sections/boring/boring.component.ts
+++ src/app/sections/boring/boring.component.ts
@@ -1,9 +1,55 @@
 import { Component } from '@angular/core';
 
+interface HistoryStep {
+  startDate: Date;
+  endDate: Date;
+  company: string;
+  role: string;
+}
+
 @Component({
   selector: 'section[boring]',
   imports: [],
   templateUrl: './boring.component.html',
   styleUrl: './boring.component.scss',
 })
-export class BoringComponent {}
+export class BoringComponent {
+  careerSteps: HistoryStep[] = [
+    {
+      startDate: new Date(2024, 0),
+      endDate: new Date(Date.now()),
+      company: 'XTel',
+      role: 'Senior front-end developer',
+    },
+    {
+      startDate: new Date(2021, 0),
+      endDate: new Date(2024, 0),
+      company: 'Tekne / Var Group',
+      role: 'Front-end architect / senior developer',
+    },
+    {
+      startDate: new Date(2018, 0),
+      endDate: new Date(2021, 0),
+      company: 'YNAP / Yoox',
+      role: 'Senior front-end developer',
+    },
+    {
+      startDate: new Date(2015, 0),
+      endDate: new Date(2018, 0),
+      company: 'Life Longari Loman',
+      role: 'Digital art director - UX/UI designer',
+    },
+    {
+      startDate: new Date(2013, 0),
+      endDate: new Date(2015, 0),
+      company: 'Neri Wolff - Quadrante',
+      role: 'UX/UI designer',
+    },
+    {
+      startDate: new Date(2015, 0),
+      endDate: new Date(2018, 0),
+      company: 'Net Sinergy',
+      role: 'UX/UI designer - Front-end developer',
+    },
+  ];
+}
