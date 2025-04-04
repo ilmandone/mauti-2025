f5bd93bb116756ca68ef5f5016478737fc9a9321
Andrea Mandini <a.mandini@mauti.it>
2025-04-04 20:31:03 +0200

    feat (social-links): change link labels by screen resolution

--- src/app/components/social-links/social-links.component.ts
+++ src/app/components/social-links/social-links.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { ScreenService } from '../../shared/services/screen.service';
+import { socialTextMap } from '@components/social-links/social_links.data';
 
 @Component({
   selector: 'social-links',
   imports: [],
   template: `
-    <a href="https://t.me/Aman1975" target="_blank">Telegram</a>
+    <a href="https://t.me/Aman1975" target="_blank">{{ currentTextMap.get('te') }}</a>
     <a href="mailto:info@mauti.it" target="_blank">&#64;</a>
-    <a href="https://www.behance.net/mandinia" target="_blank">Behance</a>
-    <a href="https://www.instagram.com/andreamandini/" target="_blank">Linkedin</a>
+    <a href="https://www.behance.net/mandinia" target="_blank">{{ currentTextMap.get('be') }}</a>
+    <a href="https://www.instagram.com/andreamandini/" target="_blank">{{ currentTextMap.get('l') }}</a>
   `,
   styles: `
     :host {
@@ -21,4 +23,17 @@ import { Component } from '@angular/core';
     }
   `,
 })
-export class SocialLinksComponent {}
+export class SocialLinksComponent {
+  private _screen = inject(ScreenService);
+
+  currentTextMap!: Map<string, string>;
+
+  constructor() {
+    effect(() => {
+      const rt = this._screen.relativeTo('d')();
+      const lm = socialTextMap.get(rt === 'after' ? 'long' : 'short');
+
+      if (lm) this.currentTextMap = lm;
+    });
+  }
+}
