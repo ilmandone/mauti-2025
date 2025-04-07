986108ea46dc4a5bacccfba57b9b6ad74b2c6f39
Andrea Mandini <a.mandini@mauti.it>
2025-04-07 20:54:03 +0200

    feat: add lenis to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ScreenService } from './shared/services/screen.service';
+import { LenisService } from './shared/services/lenis.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,10 @@ import { ScreenService } from './shared/services/screen.service';
 })
 export class AppComponent implements OnInit {
   private _screenSrv = inject(ScreenService);
+  private _lenis = inject(LenisService);
 
   ngOnInit() {
     this._screenSrv.init();
+    this._lenis.init();
   }
 }
