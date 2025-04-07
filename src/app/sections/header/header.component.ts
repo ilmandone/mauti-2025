2e3eb9fd05d65f7c4049ae8ef7560f3dbb0a106e
Andrea Mandini <a.mandini@mauti.it>
2025-04-07 21:44:40 +0200

    feat (header): mini intro (#13)

--- src/app/sections/header/header.component.ts
+++ src/app/sections/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component, HostBinding, inject } from '@angular/core';
+import { AfterViewInit, Component, HostBinding, inject } from '@angular/core';
 import { SocialLinksComponent } from '@components/social-links/social-links.component';
 import { StateService } from '../../shared/services/state.service';
+import { INTRO_DELAY_TIME } from '../../shared/commons';
 
 @Component({
   selector: 'header[section]',
@@ -11,10 +12,20 @@ import { StateService } from '../../shared/services/state.service';
   styleUrl: './header.component.scss',
   imports: [SocialLinksComponent],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   private _state = inject(StateService);
 
-  @HostBinding('class.hidden') get atBottom() {
+  @HostBinding('class.ready')
+  ready = false;
+
+  @HostBinding('class.hidden')
+  get atBottom() {
     return this._state.atBottom();
+  }
+
+  ngAfterViewInit() {
+    window.setTimeout(() => {
+      this.ready = true;
+    }, INTRO_DELAY_TIME);
   }
 }
