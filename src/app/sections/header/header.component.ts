0747e0462267cd813f30d41bd897f80a6a23e9fa
Andrea Mandini <a.mandini@mauti.it>
2025-04-07 20:30:17 +0200

    feat (header): hide at page bottom

--- src/app/sections/header/header.component.ts
+++ src/app/sections/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, inject } from '@angular/core';
 import { SocialLinksComponent } from '@components/social-links/social-links.component';
+import { StateService } from '../../shared/services/state.service';
 
 @Component({
   selector: 'header[section]',
-  templateUrl: './header.component.html',
+  template: `
+    <social-links />
+    <div class="extra">1.0 - MVP</div>
+  `,
   styleUrl: './header.component.scss',
   imports: [SocialLinksComponent],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private _state = inject(StateService);
+
+  @HostBinding('class.hidden') get atBottom() {
+    return this._state.atBottom();
+  }
+}
