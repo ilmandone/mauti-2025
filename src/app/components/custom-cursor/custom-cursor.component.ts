c834bc540a5448e997184fb8181b91e08e2122ac
Andrea Mandini <a.mandini@mauti.it>
2025-04-08 19:25:32 +0200

    perf (custom-cursor): improve easing effect for trailing

--- src/app/components/custom-cursor/custom-cursor.component.ts
+++ src/app/components/custom-cursor/custom-cursor.component.ts
@@ -14,41 +14,41 @@ interface Coords {
   styleUrl: './custom-cursor.component.scss',
 })
 export class CustomCursorComponent implements OnInit, OnDestroy {
+  private readonly EASING_FACTOR = 40;
   private _destroyRef = inject(DestroyRef);
   private _element = inject(ElementRef);
   private _raf!: number;
   private _targetCoords: Coords = { x: 0, y: 0 };
-  private _currentCoords: Coords = { x: 0, y: 0 };
-  private _angle: number = 0;
-  private readonly _orbitRadius: number = 32;
+  private _currentCoords: Coords = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+  private _angle = 0;
+  private _minDistance = 48;
 
   @ViewChild('content', { read: ElementRef, static: true }) private _content!: ElementRef<HTMLElement>;
 
   private _update() {
-    this._currentCoords.x = this._currentCoords.x + (this._targetCoords.x - this._currentCoords.x) / 40;
-    this._currentCoords.y = this._currentCoords.y + (this._targetCoords.y - this._currentCoords.y) / 40;
-
     const dx = this._targetCoords.x - this._currentCoords.x;
     const dy = this._targetCoords.y - this._currentCoords.y;
 
-    if (Math.abs(dx) > 0.1 || Math.abs(dy) > 0.1) {
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance > 0.1) {
       this._angle = Math.atan2(dy, dx);
     }
 
-    const distance = Math.sqrt(dx * dx + dy * dy);
+    let easingFactor = this.EASING_FACTOR;
 
-    let finalX = this._currentCoords.x;
-    let finalY = this._currentCoords.y;
+    if (distance < this._minDistance * 2) {
+      easingFactor = this.EASING_FACTOR + (2 * this._minDistance - distance) * 2;
+    }
 
-    if (distance < this._orbitRadius) {
-      const ratio = 1 - this._orbitRadius / distance;
-      finalX = this._currentCoords.x + dx * ratio;
-      finalY = this._currentCoords.y + dy * ratio;
+    if (distance > this._minDistance) {
+      this._currentCoords.x += dx / easingFactor;
+      this._currentCoords.y += dy / easingFactor;
     }
 
     const angleInDegrees = (this._angle * 180) / Math.PI;
 
-    this._element.nativeElement.style.transform = `translate(${finalX}px, ${finalY}px)`;
+    this._element.nativeElement.style.transform = `translate(${this._currentCoords.x}px, ${this._currentCoords.y}px)`;
     this._content.nativeElement.style.transform = `translate(-50%, -50%) rotate(${angleInDegrees}deg)`;
 
     this._raf = requestAnimationFrame(this._update.bind(this));
