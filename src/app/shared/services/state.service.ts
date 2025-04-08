3b0852f27e3c52ea9a8db84cbb2e68e8e3f618f3
Andrea Mandini <a.mandini@mauti.it>
2025-04-08 19:40:33 +0200

    feat (state): atTop

--- src/app/shared/services/state.service.ts
+++ src/app/shared/services/state.service.ts
@@ -5,6 +5,11 @@ import { Injectable, signal } from '@angular/core';
 })
 export class StateService {
   private _atBottom = signal<boolean>(false);
+  private _atTop = signal<boolean>(true);
+
+  get atTop() {
+    return this._atTop.asReadonly();
+  }
 
   get atBottom() {
     return this._atBottom.asReadonly();
@@ -12,5 +17,9 @@ export class StateService {
 
   setAtBottom(v: boolean) {
     this._atBottom.set(v);
+  }
+
+  setAtTop(v: boolean) {
+    this._atTop.set(v);
   }
 }
