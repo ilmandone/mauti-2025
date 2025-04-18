f152f36889527c0e04381aad07318d1df5215e37
Andrea Mandini <a.mandini@mauti.it>
2025-04-18 17:52:13 +0200

    feat: test simple loop animation

--- src/app/sections/hello/hello.component.ts
+++ src/app/sections/hello/hello.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostBinding, inject, NgZone, OnInit } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { createTimeline } from 'animejs';
 
 @Component({
   selector: 'section[hello]',
@@ -7,12 +8,29 @@ import { NgOptimizedImage } from '@angular/common';
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.scss',
 })
-export class HelloComponent implements OnInit {
+export class HelloComponent implements OnInit, AfterViewInit {
+  private _ngZone = inject(NgZone);
+
   @HostBinding('class.visible') visible = false;
 
   ngOnInit() {
     window.setTimeout(() => {
       this.visible = true;
     }, 0);
+  }
+
+  ngAfterViewInit() {
+    window.setTimeout(() => {
+      const tl = createTimeline({ loop: true });
+
+      tl.add('.hand', { rotate: 15, duration: 150 })
+        .add('.hand', { rotate: 7, duration: 200 })
+        .add('.hand', {
+          rotate: 12,
+          duration: 150,
+        })
+        .add('.hand', { rotate: 0, duration: 600 })
+        .add({ duration: 1000 });
+    }, 1000);
   }
 }
