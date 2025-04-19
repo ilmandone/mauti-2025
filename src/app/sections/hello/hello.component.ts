327291a9be8067093f81edfd863207f739e7b4fa
Andrea Mandini <a.mandini@mauti.it>
2025-04-19 16:25:57 +0200

    Feature/hello animation animejs (#17)
    * chore: install animeJs
    
    * chore: add types for animejs
    
    * feat: test simple loop animation
    
    * feat: scroll to pause the animation
    
    * fix: remove unused
    
    * chore: update favicon
    
    * feat (hello): update animation and remove debug
    
    * refactoring (hello): extract animatio in function

--- src/app/sections/hello/hello.component.ts
+++ src/app/sections/hello/hello.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostBinding, OnInit } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { createTimeline, onScroll } from 'animejs';
 
 @Component({
   selector: 'section[hello]',
@@ -7,12 +8,39 @@ import { NgOptimizedImage } from '@angular/common';
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.scss',
 })
-export class HelloComponent implements OnInit {
+export class HelloComponent implements OnInit, AfterViewInit {
   @HostBinding('class.visible') visible = false;
+
+  private _handAnimation() {
+    const tl = createTimeline({
+      loop: true,
+      autoplay: onScroll({
+        target: '.hand',
+        enter: 'bottom top',
+        leave: 'top bottom',
+      }),
+    });
+
+    tl.add('.hand', { rotate: 12, duration: 150 })
+      .add('.hand', { rotate: 4, duration: 200 })
+      .add('.hand', {
+        rotate: 12,
+        duration: 150,
+      })
+      .add({ duration: 100 })
+      .add('.hand', { rotate: 0, duration: 400 })
+      .add({ duration: 1200 });
+  }
 
   ngOnInit() {
     window.setTimeout(() => {
       this.visible = true;
     }, 0);
+  }
+
+  ngAfterViewInit() {
+    window.setTimeout(() => {
+      this._handAnimation();
+    }, 1000);
   }
 }
