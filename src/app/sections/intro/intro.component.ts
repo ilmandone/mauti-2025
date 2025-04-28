d27b28a76c1f3061ce23a74489de7e352f399d8f
Andrea Mandini <a.mandini@mauti.it>
2025-04-28 22:18:05 +0200

    feat (intro): huge text intro animation

--- src/app/sections/intro/intro.component.ts
+++ src/app/sections/intro/intro.component.ts
@@ -1,6 +1,8 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, NgZone, viewChild } from '@angular/core';
 import { ColorCardComponent } from '@components/color-card/color-card.component';
 import { LogoBlockComponent } from '@components/logo-block/logo-block.component';
+import { animate } from 'animejs';
+import { getHugeTextAnimationOptions } from '../../shared/commons';
 
 @Component({
   selector: 'section[intro]',
@@ -8,6 +10,18 @@ import { LogoBlockComponent } from '@components/logo-block/logo-block.component'
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss',
 })
-export class IntroComponent {
+export class IntroComponent implements AfterViewInit {
+  private _ngZone = inject(NgZone);
+
   aboutEl = viewChild<ElementRef<HTMLElement>>('about');
+
+  private _setAnimation() {
+    animate('.huge__block', getHugeTextAnimationOptions());
+  }
+
+  ngAfterViewInit() {
+    this._ngZone.runOutsideAngular(() => {
+      this._setAnimation();
+    });
+  }
 }
--- src/app/shared/commons.ts
+++ src/app/shared/commons.ts
@@ -1,1 +1,17 @@
+import { onScroll, stagger } from 'animejs';
+
 export const INTRO_DELAY_TIME = 140;
+
+export const getHugeTextAnimationOptions = (target: string = '.huge') => {
+  return {
+    delay: stagger(100, { start: 200 }),
+    duration: stagger(1000, { start: 600 }),
+    opacity: [{ from: 0 }, { to: 1 }],
+    y: [{ from: 60 }, { to: 0 }],
+    autoplay: onScroll({
+      target,
+      enter: 'bottom top',
+      leave: 'top bottom',
+    }),
+  };
+};
