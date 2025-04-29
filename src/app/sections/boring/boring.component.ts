f523d97b527df1c684a03f9db919fcdcddc57d60
Andrea Mandini <a.mandini@mauti.it>
2025-04-29 22:06:34 +0200

    feat (boring): split skills in span list and setup for animations

--- src/app/sections/boring/boring.component.ts
+++ src/app/sections/boring/boring.component.ts
@@ -1,11 +1,5 @@
-import { Component } from '@angular/core';
-
-interface HistoryStep {
-  startDate: Date;
-  endDate: Date;
-  company: string;
-  role: string;
-}
+import { AfterViewChecked, Component, inject, NgZone } from '@angular/core';
+import { careerSteps } from './boring.configs';
 
 @Component({
   selector: 'section[boring]',
@@ -13,43 +7,18 @@ interface HistoryStep {
   templateUrl: './boring.component.html',
   styleUrl: './boring.component.scss',
 })
-export class BoringComponent {
-  careerSteps: HistoryStep[] = [
-    {
-      startDate: new Date(2024, 0),
-      endDate: new Date(Date.now()),
-      company: 'XTel',
-      role: 'Senior front-end developer',
-    },
-    {
-      startDate: new Date(2021, 0),
-      endDate: new Date(2024, 0),
-      company: 'Tekne / Var Group',
-      role: 'Front-end architect / senior developer',
-    },
-    {
-      startDate: new Date(2018, 0),
-      endDate: new Date(2021, 0),
-      company: 'YNAP / Yoox',
-      role: 'Senior front-end developer',
-    },
-    {
-      startDate: new Date(2015, 0),
-      endDate: new Date(2018, 0),
-      company: 'Life Longari Loman',
-      role: 'Digital art director - UX/UI designer',
-    },
-    {
-      startDate: new Date(2013, 0),
-      endDate: new Date(2015, 0),
-      company: 'Neri Wolff - Quadrante',
-      role: 'UX/UI designer',
-    },
-    {
-      startDate: new Date(2015, 0),
-      endDate: new Date(2018, 0),
-      company: 'Net Sinergy',
-      role: 'UX/UI designer - Front-end developer',
-    },
-  ];
+export class BoringComponent implements AfterViewChecked {
+  private _ngZone = inject(NgZone);
+
+  careerSteps = careerSteps;
+
+  private _setAnimation() {
+    console.log('START BORING ANIMATIONS');
+  }
+
+  ngAfterViewChecked() {
+    this._ngZone.runOutsideAngular(() => {
+      this._setAnimation();
+    });
+  }
 }
