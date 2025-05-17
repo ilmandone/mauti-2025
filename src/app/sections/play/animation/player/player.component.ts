31d033f813288efc71f38ae56e37825924551626
Andrea Mandini <a.mandini@mauti.it>
2025-05-17 16:42:51 +0200

    Feature/play 3d animation (#21)
    * chore: update logo squared pict
    
    * chore: update packges and install babylon-core
    
    * feat: basic babylon setup with player component and animation class
    
    * chore: remove unused test
    
    * feat (player): animate setup to control babylon animation
    
    * feat (player): sync render tick with scroll interceptor
    
    * feat (player): start animation definition
    
    * feat (player): refine animation and transitions
    
    * feat (player): colors on instances
    
    * feat: finalize animations and restore sections
    
    * feat: add black color to boxes
    
    * feat: camera rotation refinements
    
    * feat (babylon-animation): change camera target
    
    * perf (babylon-animation): render only on progress
    
    * chore: remove old play bg asset and img

---
+++ src/app/sections/play/animation/player/player.component.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, Component, ElementRef, inject, NgZone, OnDestroy, OnInit, viewChild } from '@angular/core';
+import { BabylonAnimation } from '../babylon-animation';
+import { animate, onScroll } from 'animejs';
+
+@Component({
+  selector: 'player-babylon',
+  imports: [],
+  templateUrl: './player.component.html',
+  styleUrl: './player.component.scss',
+})
+export class PlayerComponent implements OnInit, AfterViewInit, OnDestroy {
+  private _ngZone = inject(NgZone);
+
+  private _canvasEl = viewChild.required<ElementRef<HTMLCanvasElement>>('canvasElement');
+  private _animation!: BabylonAnimation;
+
+  private _setAnimation() {
+    animate(this._canvasEl().nativeElement, {
+      progress: [{ from: 0 }, { to: 100 }],
+      autoplay: onScroll({
+        enter: 'bottom top',
+        leave: 'top bottom',
+        sync: true,
+        onUpdate: (e) => {
+          this._animation.progress(e.progress);
+        },
+      }),
+    });
+  }
+
+  ngOnInit() {
+    this._animation = new BabylonAnimation(this._canvasEl().nativeElement);
+    this._animation.init();
+  }
+
+  ngAfterViewInit() {
+    this._ngZone.runOutsideAngular(() => {
+      this._setAnimation();
+    });
+  }
+
+  ngOnDestroy() {
+    this._animation.destroy();
+  }
+}
