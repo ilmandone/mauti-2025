0cad0fdfe3b5b95cbb8b46c4322c39e7437d72fd
Andrea Mandini <a.mandini@mauti.it>
2025-05-17 18:38:18 +0200

    feat (main-loading): finalize loading content

--- src/app/components/main-loading/main-loading.component.ts
+++ src/app/components/main-loading/main-loading.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-main-loading',
   imports: [],
   template: `
-    <h1><span class="sec">\\(</span><span class="primary">°Д°</span><span class="sec">)/</span></h1>
-    <div class="dots">...</div>
+    <h1><span class="sec">\\(</span><span class="primary">^Д^</span><span class="sec">)/</span></h1>
+
+    <div class="dots">{{ dots }}</div>
+    <div class="message">JUST A SECOND</div>
   `,
   styleUrl: './main-loading.component.scss',
 })
-export class MainLoadingComponent {}
+export class MainLoadingComponent implements OnInit {
+  private readonly MAX_DOTS: number = 10;
+
+  private _destroyRef = inject(DestroyRef);
+  private _count: number = 1;
+  private _isIncreasing: boolean = true;
+
+  dots: string = '.';
+
+  ngOnInit() {
+    interval(200)
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe(() => {
+        if (this._isIncreasing) {
+          this._count++;
+          if (this._count >= this.MAX_DOTS) {
+            this._isIncreasing = false;
+          }
+        } else {
+          this._count--;
+          if (this._count <= 1) {
+            this._isIncreasing = true;
+          }
+        }
+
+        this.dots = '.'.repeat(this._count);
+      });
+  }
+}
