713d88d80845b74a7af19b3aa2add64887cc2437
Andrea Mandini <a.mandini@mauti.it>
2025-07-11 16:58:52 +0200

    feat: (header) click2 on sound button

--- src/app/sections/header/header.component.ts
+++ src/app/sections/header/header.component.ts
@@ -3,13 +3,14 @@ import { SocialLinksComponent } from '@components/social-links/social-links.comp
 import { StateService } from '../../shared/services/state.service';
 import { INTRO_DELAY_TIME } from '../../shared/commons';
 import { ToggleButtonComponent } from '@components/toggle-button/toggle-button.component';
+import { AudioService } from '../../shared/services/audio.service';
 
 @Component({
   selector: 'header[section]',
   template: `
     <social-links />
     <div class="extra">
-      <app-toggle-button>
+      <app-toggle-button (click)="toggleSound($event)">
         <ng-template #off>Sounssssd</ng-template>
         <ng-template #on>Attivo</ng-template>
       </app-toggle-button>
@@ -21,6 +22,7 @@ import { ToggleButtonComponent } from '@components/toggle-button/toggle-button.c
 })
 export class HeaderComponent implements AfterViewInit {
   private _state = inject(StateService);
+  private _audio = inject(AudioService);
 
   @HostBinding('class.ready')
   ready = false;
@@ -28,6 +30,12 @@ export class HeaderComponent implements AfterViewInit {
   @HostBinding('class.hidden')
   get atBottom() {
     return this._state.atBottom() || !this.ready;
+  }
+
+  toggleSound($event: boolean) {
+    this._state.setSoundsOn($event);
+    console.log($event);
+    if ($event) this._audio.play('click2');
   }
 
   ngAfterViewInit() {
