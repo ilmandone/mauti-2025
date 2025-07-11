c2c18b0d3b55dc17f9878d51c73032e6961aa884
Andrea Mandini <a.mandini@mauti.it>
2025-07-11 12:26:45 +0200

    chore: (header) update version

--- src/app/sections/header/header.component.ts
+++ src/app/sections/header/header.component.ts
@@ -7,7 +7,7 @@ import { INTRO_DELAY_TIME } from '../../shared/commons';
   selector: 'header[section]',
   template: `
     <social-links />
-    <div class="extra">1.2 - ANIMATIONS</div>
+    <div class="extra">1.3 - SOUNDS</div>
   `,
   styleUrl: './header.component.scss',
   imports: [SocialLinksComponent],
