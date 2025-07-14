6f0007f5c7a52d05cab8c8d836687d2844242526
Andrea Mandini <a.mandini@mauti.it>
2025-07-14 19:45:36 +0200

    feat: (boring) update career animation elements and add sounds

--- src/app/sections/boring/boring.component.ts
+++ src/app/sections/boring/boring.component.ts
@@ -1,8 +1,10 @@
 import { AfterViewInit, Component, ElementRef, inject, NgZone, viewChild } from '@angular/core';
 import { careerSteps } from './boring.configs';
-import { animate, onScroll, stagger, utils } from 'animejs';
+import { animate, createTimeline, onScroll, stagger, utils } from 'animejs';
 
 import { getDefaultScrollObs } from '../../shared/animejs';
+import { AudioService } from '../../shared/services/audio.service';
+import { StateService } from '../../shared/services/state.service';
 
 @Component({
   selector: 'section[boring]',
@@ -13,6 +15,9 @@ import { getDefaultScrollObs } from '../../shared/animejs';
 export class BoringComponent implements AfterViewInit {
   private _elRef = inject(ElementRef);
   private _ngZone = inject(NgZone);
+  private _audio = inject(AudioService);
+  private _state = inject(StateService);
+
   private _career = viewChild<ElementRef<HTMLElement>>('career');
 
   careerSteps = careerSteps;
@@ -29,18 +34,30 @@ export class BoringComponent implements AfterViewInit {
       autoplay: getDefaultScrollObs(this._elRef.nativeElement),
     });
 
-    const careerItems = this._elRef.nativeElement.querySelectorAll('.career__item');
-    animate(careerItems, {
-      opacity: [{ from: 0 }, { to: 1 }],
-      rotateX: [{ from: -60 }, { to: 0 }],
-      delay: stagger(150),
-      ease: 'outElastic',
-      duration: 2500,
+    const careerItems = this._elRef.nativeElement.querySelectorAll('.career__item') as NodeList;
+
+    const tl = createTimeline({
       autoplay: onScroll({
         target: this._career()?.nativeElement,
         enter: '90% top',
         leave: '10% bottom',
       }),
+    });
+
+    (Array.from(careerItems) as HTMLElement[]).forEach((el: HTMLElement, index: number) => {
+      const a = animate(el, {
+        opacity: [{ from: 0 }, { to: 1 }],
+        rotateX: [{ from: -60 }, { to: 0 }],
+        ease: 'outElastic',
+        duration: 2500,
+        onBegin: () => {
+          if (this._state.soundsOn()) {
+            this._audio.play('click2');
+          }
+        },
+      });
+
+      tl.sync(a, 150 * index);
     });
   }
 
