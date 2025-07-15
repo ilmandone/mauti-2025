981148b25bb6b35ec0c76f34c0ba51dedacae082
Andrea Mandini <a.mandini@mauti.it>
2025-07-15 21:29:46 +0200

    feat: (footer) add sounds button and change row order

--- src/app/sections/footer/footer.component.ts
+++ src/app/sections/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SocialLinksComponent } from '@components/social-links/social-links.component';
+import { SoundButtonComponent } from '@components/sound-button/sound-button.component';
 
 @Component({
   selector: 'footer[section]',
-  imports: [SocialLinksComponent],
+  imports: [SocialLinksComponent, SoundButtonComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
